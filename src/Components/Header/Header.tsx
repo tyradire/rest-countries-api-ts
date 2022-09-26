f34d5027b46d740cf715f5c4540977c4c6d696b6
Daniel Ugarov <tyradire@yandex.ru>
2022-09-26 17:00:09 +0300

    fix routes

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <div className='header'>
       <nav className='header__container'>
-        <Link to='/' className='header__title'>
+        <Link to='/rest-countries-api-ts/' className='header__title'>
           <h1>Where in the world?</h1>
         </Link>
         <ThemeSwitcher />
